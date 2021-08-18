b9527f4c867edb2727ffe8a2075163edbfb8f10e
ksaweryr <ksawery.radziwilowicz@gmail.com>
2021-08-18 15:41:53 +0200

    Optimize code

--- snake.asm
+++ snake.asm
@@ -28,26 +28,34 @@ org 0x7c00
 ; CONSTANTS
 ; ---------
 ; Absolute memory addresses
-VIDMEM equ 0xb800               ; video memory address as memory segment
-SNAKEMEM equ 0x50               ; game's memory address as memory segment
-TIMER equ 0x46c                 ; address of system time
+VIDMEM equ 0xb800                       ; video memory address as memory segment
+SNAKEMEM equ 0x050                       ; game's memory address as memory segment
+TIMER equ 0x46c                         ; address of system time
 
 ; Game data memory addresses (relative to SNAKEMEM)
-FOOD_POS equ 0x00               ; address of food position
-SNAKE_SPEED equ 0x02            ; address of snake's speed
-SNAKE_LEN equ 0x04              ; address of snake's length
-LCG_X equ 0x06                  ; address of current pseudo random number
-SZ_SNAKE_LEN equ 0x08           ; address of snake's length string representation
+FOOD_POS equ 0x00                       ; address of food position
+SNAKE_SPEED equ 0x02                    ; address of snake's speed
+SNAKE_LEN equ 0x04                      ; address of snake's length
+LCG_X equ 0x06                          ; address of current pseudo random number
+SZ_SNAKE_LEN equ 0x08                   ; address of snake's length string representation
 ALIVE equ 0x0d
 STACK_BOTTOM equ 0x0e
 
 ; Other constants
-ROW_LENGTH equ 0x50             ; board width
-COL_HEIGHT equ 0x18             ; board height
+ROW_LENGTH equ 0x50                     ; board width
+COL_HEIGHT equ 0x18                     ; board height
 DATA_LEN equ STACK_BOTTOM + (ROW_LENGTH * COL_HEIGHT * 2) + 2
-                                ; length of game's data; relative address of the end of data
-LCG_A equ 0x61                  ; LGC a value
-LCG_C equ 0x59                  ; LGC c value
+                                        ; length of game's data
+LCG_A equ 0x61                          ; LGC a value
+LCG_C equ 0x59                          ; LGC c value
+
+; MACROS
+; ------
+; Changes character and colour to a single word which can be moved directly to
+; video memory
+; Note: in video memory character comes before colour but since x86 is little-endian,
+; the colour is in the high-order byte
+%define printable(char, colour) (colour << 8 | char)
 
 ; CODE
 ; ----
@@ -55,47 +63,45 @@ start:
     xor ax, ax
     mov ds, ax
     mov es, ax
-    mov fs, ax                  ; clear segments registers
+    mov fs, ax                          ; clear segments registers
 
 init_data:
     mov ax, SNAKEMEM
     mov ds, ax
     mov es, ax
-    mov ss, ax                  ; data segment, extra segment, stack segment = SNAKEMEM
+    mov ss, ax                          ; data segment, extra segment, stack segment = SNAKEMEM
     mov cx, DATA_LEN
     xor ax, ax
     mov di, 0x00
-    .fill:
-        stosb
-        loop .fill              ; fill game's memory with 0
+    rep stosb                           ; fill game's memory with 0
 
-    mov word [SNAKE_LEN], 1     ; set snake's length
+    mov word [SNAKE_LEN], 0x01          ; set snake's length
     mov dx, word [fs:TIMER]
-    mov word [LCG_X], dx        ; set seed of LGC
-    call move_food              ; place food
-    mov byte [ALIVE], 1
+    mov word [LCG_X], dx                ; set seed of LGC
+    call move_food                      ; place food
+    mov byte [ALIVE], 0x01
 
 init_stack:
-    mov bp, DATA_LEN            ; the base pointer points to snake's head
+    mov bp, DATA_LEN                    ; the base pointer points to snake's head
     mov sp, bp
     mov ax, 0x280d
-    push ax                     ; push snake's initial position (40, 13)
+    push ax                             ; push snake's initial position (40, 13)
 
 init_video:
     xor ah, ah
     mov al, 0x03
-    int 0x10                    ; set video mode: 80x25 chars, 16-color
+    int 0x10                            ; set video mode: 80x25 chars, 16-color
 
     mov ax, VIDMEM
-    mov es, ax                  ; set es to point at video memory
+    mov es, ax                          ; set es to point at video memory
 
 game_loop:
     read_key:
         mov ah, 0x01
-        int 0x16                    ; check buffer status
+        int 0x16                        ; check buffer status
         jz calculate_new_head_pos
         xor ah, ah
-        int 0x16                    ; get keystroke
+        int 0x16                        ; get keystroke
         cmp al, 'w'
         je handle_input.w
         cmp al, 's'
@@ -113,12 +119,12 @@ game_loop:
             cmp dl, 0x01
             je calculate_new_head_pos
             xor dh, dh
-            mov dl, -1
+            mov dl, -0x01
             mov word [SNAKE_SPEED], dx
             jmp calculate_new_head_pos
         .s:
             mov dx, word [SNAKE_SPEED]
-            cmp dl, -1
+            cmp dl, -0x01
             je calculate_new_head_pos
             xor dh, dh
             mov dl, 0x01
@@ -129,19 +135,19 @@ game_loop:
             cmp dh, 0x01
             je calculate_new_head_pos
             xor dl, dl
-            mov dh, -1
+            mov dh, -0x01
             mov word [SNAKE_SPEED], dx
             jmp calculate_new_head_pos
         .d:
             mov dx, word [SNAKE_SPEED]
-            cmp dh, -1
+            cmp dh, -0x01
             je calculate_new_head_pos
             xor dl, dl
             mov dh, 0x01
             mov word [SNAKE_SPEED], dx
 
     calculate_new_head_pos:
-        mov bx, word [bp - 0x02]    ; head position
+        mov bx, word [bp - 0x02]        ; head position
         mov cx, word [SNAKE_SPEED]
 
         .move_head_vertically:
@@ -173,16 +179,16 @@ game_loop:
         mov cx, word [SNAKE_LEN]
         dec cx
         test cx, cx
-        jz move_head           ; skip if snake's length is 1
+        jz move_head                    ; skip if snake's length is 1
         .loop:
             mov dx, word [si + 0x02]
-            cmp dx, bx              ; bx holds head position
+            cmp dx, bx                  ; bx holds head position
             jnz .store
-            mov byte [ALIVE], 0     ; end the game if head hits snake's body
+            mov byte [ALIVE], 0x00      ; end the game if head hits snake's body
             .store:
-            mov word [si], dx       ; set nth segment new position to (n-1)th
-                                    ; segment old position
-            add si, 2
+            mov word [si], dx           ; set nth segment new position to (n-1)th
+                                        ; segment old position
+            add si, 0x02
         loop .loop
 
     move_head:
@@ -195,46 +201,36 @@ game_loop:
         inc word [SNAKE_LEN]
         mov si, sp
         mov bx, word [si]
-        push bx                 ; add new snake segment
-        call move_food          ; relocate food
+        push bx                         ; add new snake segment
+        call move_food                  ; relocate food
 
     clear_board:
         xor di, di
         mov cx, COL_HEIGHT * ROW_LENGTH
-        .loop:
-            mov al, ' '
-            stosb
-            mov al, 0x00            ; colour: black
-            stosb
-            loop .loop
+        mov ax, printable(' ', 0x00)    ; colour: black
+        rep stosw
 
     draw_snake:
         mov si, sp
-        mov bl, byte [ALIVE]
-        xor bl, 1               ; bl = 0 if alive; else 1
-        shl bl, 1
-        add bl, 0x02            ; colour: green if alive; else red
+        mov bh, byte [ALIVE]
+        xor bh, 1                       ; bl = 0 if alive; else 1
+        shl bh, 1
+        add bh, 0x0a                    ; colour: bright green if alive; else bright red
+        mov cx, word [SNAKE_LEN]
         .loop:
             call coords_to_addr
             add si, 0x02
-            mov al, 0xfe        ; CCSID 437 character: black square
-            stosb
-            mov al, bl
-            cmp si, bp
-            jz .end
-            add al, 0x08        ; brighten all segments except the head
-            stosb
-            jmp .loop
-            .end:
-            stosb
+            mov ah, bh
+            mov al, 0xfe                ; CCSID 437 character: black square
+            stosw
+            loop .loop
+        sub byte [es:di - 0x01], 0x08   ; darken the head
 
     draw_food:
         lea si, word [FOOD_POS]
         call coords_to_addr
-        mov al, 0xfe            ; CCSID 437 character: black square
-        stosb
-        mov al, 0x05            ; colour: purple
-        stosb
+        mov ax, printable(0x04, 0x05)   ; CCSID 437 character: diamond; colour: purple
+        stosw
 
     print_score:
         mov ax, word [SNAKE_LEN]
@@ -252,14 +248,13 @@ game_loop:
             test al, al
             jnz .itoa_loop
         mov si, di
-        mov di, (ROW_LENGTH * (COL_HEIGHT + 1) - 5) * 2
+        mov di, (ROW_LENGTH * (COL_HEIGHT + 0x01) - 0x05) * 0x02
         .loop:
             lodsb
             test al, al
             jz end_if_dead
-            stosb
-            mov al, 0x03        ; colour: cyan
-            stosb
+            mov ah, 0x03                ; colour: cyan
+            stosw
             jmp .loop
 
     end_if_dead:
@@ -269,7 +264,7 @@ game_loop:
 
     delay:
         mov bx, [fs:TIMER]
-        add bx, 2               ; wait for 2 ticks
+        add bx, 0x02                    ; wait for 2 ticks
         .wait:
             cmp word [fs:TIMER], bx
             jl .wait
@@ -280,8 +275,8 @@ end:
     .await_restart:
         mov ah, 0x01
         int 0x16
-        jz .await_restart           ; do nothing until any key is pressed
-    jmp start                       ; restart the game
+        jz .await_restart               ; do nothing until any key is pressed
+    jmp start                           ; restart the game
 
 
 ; FUNCTIONS
@@ -315,11 +310,11 @@ move_food:
     movzx ax, al
     mov dl, COL_HEIGHT
     div dl
-    mov cl, ah                      ; new food position's y
+    mov cl, ah                          ; new food position's y
     movzx ax, ch
     mov dl, ROW_LENGTH
     div dl
-    mov ch, ah                      ; new food position's x
+    mov ch, ah                          ; new food position's x
     mov word [FOOD_POS], cx
     ret
 
